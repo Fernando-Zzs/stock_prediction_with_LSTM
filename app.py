e892f3b56967f46d1ff5632f1aa35965ac573404
FernandoZzs <760156389@qq.com>
2023-04-17 10:47:59 +0800

    Enriched the content of the sidebar

--- app.py
+++ app.py
@@ -25,28 +25,41 @@ st.title("股票价格预测")
 # 创建侧边栏，用于用户输入模型参数
 st.sidebar.title("自定义预测参数")
 config = Config()
-
+st.sidebar.markdown('---')
 config.stock_code = st.sidebar.text_input("股票代码", value='000001')
 config.start_date = datetime.strftime(
     st.sidebar.date_input("起始日期", value=gap_period(datetime.strftime(datetime.now(), "%Y%m%d"), 3, True),
                           max_value=gap_period(config.end_date, 2, True)), "%Y%m%d")
 config.end_date = datetime.strftime(st.sidebar.date_input("结束日期", value=datetime.now(),
                                                           min_value=gap_period(config.start_date, 2, False)), "%Y%m%d")
-config.fq_method = view_options[st.sidebar.radio("复权类型", ('前复权', '后复权'))]
+config.fq_method = view_options[
+    st.sidebar.radio("复权类型", ('前复权', '后复权'), horizontal=True, help="选择前复权能够更好地反映短期价格趋势，选择后复权能够更好地反映长期涨跌情况。")]
+st.sidebar.markdown('---')
 
 with st.sidebar.expander("网络参数"):
-    config.label_columns = [view_options[selected_option] for selected_option in
-                            st.sidebar.multiselect("预测项", ['开盘价', '收盘价', '最高价', '最低价'], ['收盘价'])]
     config.used_frame = st.radio("框架", ('PyTorch', 'TensorFlow'), horizontal=True)
     config.predict_day = st.slider("预测天数", min_value=1, max_value=7, step=1, value=1)
-    config.hidden_size = st.radio("隐藏层大小", (64, 128, 256), index=1, horizontal=True)
-    config.lstm_layers = st.slider("堆叠层数", min_value=1, max_value=8, step=1, value=2)
-    config.dropout_rate = st.slider("丢包率", min_value=0.0, max_value=1.0, step=0.05, value=0.2)
+    config.hidden_size = st.radio("隐藏层大小", (64, 128, 256), index=1, horizontal=True, help="网络的每层隐藏层的规模")
+    config.lstm_layers = st.slider("堆叠层数", min_value=1, max_value=8, step=1, value=2, help="网络的LSTM堆叠层数")
+    config.dropout_rate = st.slider("丢包率", min_value=0.0, max_value=1.0, step=0.05, value=0.2,
+                                    help="作用于LSTM网络之间以一定概率丢失数据，防止过拟合")
     config.time_step = st.number_input("时间步长", min_value=1, max_value=calc_bdate(config.start_date, config.end_date),
-                                       step=1, value=20)
+                                       step=1, value=20, help="用前多少天的数据来预测，也是LSTM的time step数")
 
-# TODO:运行前需要检查：所有空都有填写
-if st.sidebar.button("运行"):
+with st.sidebar.expander("训练参数"):
+    config.add_train = st.checkbox("增量训练", value=False, help="是否载入已有模型参数进行增量训练")
+    config.shuffle_train_data = st.checkbox("打乱训练", value=True, help="是否对训练数据做打乱操作")
+    config.use_cuda = st.checkbox("使用GPU训练", value=False)
+    config.do_continue_train = st.checkbox("持续训练", value=False, disabled=config.used_frame == 'TensorFlow',
+                                           help="每次训练把上一次的final_state作为下一次的init_state")
+    config.debug_mode = st.checkbox("离线模式", value=False, help="不联网获取数据，请确保数据库为最新数据")
+
+st.sidebar.markdown('---')
+config.label_columns = [view_options[selected_option] for selected_option in
+                        st.sidebar.multiselect("预测变量", ['开盘价', '收盘价', '最高价', '最低价'], ['收盘价'])]
+
+# TODO:运行前需要检查：所有空都有填写,do_continue_train变更时同时变更
+if st.sidebar.button("运行", type='primary', use_container_width=True):
     # 修改预测列需要联动修改其他参数
     config.label_in_feature_index = (lambda x, y: [x.index(i) for i in y])(config.feature_columns, config.label_columns)
     config.output_size = len(config.label_columns)
